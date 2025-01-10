3f28d9d3931935047c461e18520226cf47d3b069
Nswer <xodls0498@jj.ac.kr>
2025-01-10 15:48:10 +0900

    Update UserController.java

--- shopping/src/main/java/com/kmove/app/User/controller/UserController.java
+++ shopping/src/main/java/com/kmove/app/User/controller/UserController.java
@@ -197,6 +197,6 @@ public class UserController {
         System.out.println("UserController deleteUser End----------------------------------------------------");
 
         return "redirect:/";
-        //fuckkkkk
+        //fuckkkkkhgdhdtyd
     }
 }//END CLASS
